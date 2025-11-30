f52a90901211ba804d828f2add5f7f2cbb553ffa
Obriel <obrieljefferson@gmail.com>
2025-11-30 10:11:56 +0300

    add quiz.js file

---
+++ quiz.js
@@ -0,0 +1,22 @@
+function checkAnswer () {
+    const correctAnswer = "4";
+    const  selectedRadio = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = null;
+    if (selectedRadio) {
+    userAnswer = selectedRadio.value; // value of the chosen option
+    }
+
+    const feedbackE1 = document.getElementById("feedback"); // 3. get feedback element
+
+    // 4. compare answers and update feedback
+    if(userAnswer === correctAnswer){
+        feedbackE1.textContent = "Correct! Well done .";
+        
+    } else {
+        feedbackE1.textContent  = "That's incorrect. Try again!";
+
+    }
+}
+
+ const submitButton = document.getElementById("submit-answer");
+    submitButton.addEventListener("click", checkAnswer);
